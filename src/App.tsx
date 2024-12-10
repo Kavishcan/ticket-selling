cd7ca4d2257138705b5d4774972a09b5b521c473
Kavishcan <kavishcan2002@gmail.com>
2024-12-11 00:38:43 +0530

    Add axios and the request

--- src/App.tsx
+++ src/App.tsx
@@ -1,36 +1,152 @@
-import ButtonConfig from "./components/ButtonConfig";
-import ButtonList from "./components/ButtonList";
-import Button from "./components/ButtonList";
-import Input from "./components/Input";
-import InputButtons from "./components/InputButtons";
+import React, { useState } from "react";
+import axios from "axios";
 import LogDisplay from "./components/LogDisplay";
 import TicketDisplay from "./components/TicketDisplay";
 import NavBar from "./components/NavBar";
 
 function App() {
-  let inputFields = [
-    "Enter the Number Of Tickerts",
-    "Ticket Release Rate",
-    "Ticket Retrival Rate",
-    "Maximum Ticket Capacity",
-  ];
-  let buttonNames = ["start", "stop"];
-  let buttonFields = ["set ven", "set cus"];
-  let buttonInputs = ["set vendor count","set customer count"];
-  let buttonColor = ["primary" ,"danger"];
+  const [numberOfTickets, setNumberOfTickets] = useState("");
+  const [ticketReleaseRate, setTicketReleaseRate] = useState("");
+  const [ticketRetrievalRate, setTicketRetrievalRate] = useState("");
+  const [maxTicketCapacity, setMaxTicketCapacity] = useState("");
+  const [vendorCount, setVendorCount] = useState("");
+  const [customerCount, setCustomerCount] = useState("");
+
+  const handleInputChange =
+    (setter: React.Dispatch<React.SetStateAction<string>>) =>
+    (event: React.ChangeEvent<HTMLInputElement>) => {
+      setter(event.target.value);
+      console.log(event.target.value);
+    };
+
+  const handleSubmit = () => {
+    const data = {
+      numberOfTickets,
+      ticketReleaseRate,
+      ticketRetrievalRate,
+      maxTicketCapacity,
+    };
+
+    axios
+      .post("/api/tickets", data)
+      .then((response) => {
+        console.log("Data sent successfully:", response.data);
+      })
+      .catch((error) => {
+        console.error("There was an error sending the data:", error);
+      });
+  };
+
+  const handleVendorSubmit = () => {
+    axios
+      .post("/api/vendor-count", { vendorCount })
+      .then((response) => {
+        console.log("Vendor count sent successfully:", response.data);
+      })
+      .catch((error) => {
+        console.error("There was an error sending the vendor count:", error);
+      });
+  };
+
+  const handleCustomerSubmit = () => {
+    axios
+      .post("/api/customer-count", { customerCount })
+      .then((response) => {
+        console.log("Customer count sent successfully:", response.data);
+      })
+      .catch((error) => {
+        console.error("There was an error sending the customer count:", error);
+      });
+  };
 
   return (
     <div>
       <NavBar />
-      <div className="row ">
+      <div className="row">
         <div className="container-xxl col p-5">
-          <Input heading="Configuration Form" inputFields={inputFields} />
-          <ButtonConfig>Config</ButtonConfig>
-          <InputButtons buttonFields={buttonFields} buttonInputs={buttonInputs} />
-          <ButtonList buttonNames={buttonNames} buttonColor={buttonColor}/>
+          <h2 className="mb-4">Configuration Form</h2>
+          <form>
+            <div className="mb-3">
+              <label className="form-label">Enter the Number Of Tickets</label>
+              <input
+                type="text"
+                className="form-control"
+                value={numberOfTickets}
+                onChange={handleInputChange(setNumberOfTickets)}
+              />
+            </div>
+            <div className="mb-3">
+              <label className="form-label">Ticket Release Rate</label>
+              <input
+                type="text"
+                className="form-control"
+                value={ticketReleaseRate}
+                onChange={handleInputChange(setTicketReleaseRate)}
+              />
+            </div>
+            <div className="mb-3">
+              <label className="form-label">Ticket Retrieval Rate</label>
+              <input
+                type="text"
+                className="form-control"
+                value={ticketRetrievalRate}
+                onChange={handleInputChange(setTicketRetrievalRate)}
+              />
+            </div>
+            <div className="mb-3">
+              <label className="form-label">Maximum Ticket Capacity</label>
+              <input
+                type="text"
+                className="form-control"
+                value={maxTicketCapacity}
+                onChange={handleInputChange(setMaxTicketCapacity)}
+              />
+            </div>
+            <button
+              type="button"
+              className="btn btn-primary mt-3"
+              onClick={handleSubmit}
+            >
+              Config
+            </button>
+          </form>
+          <div className="mt-4">
+            <div className="mb-3">
+              <label className="form-label">Vendor Count</label>
+              <input
+                type="text"
+                className="form-control"
+                value={vendorCount}
+                onChange={handleInputChange(setVendorCount)}
+              />
+              <button
+                type="button"
+                className="btn btn-secondary mt-2"
+                onClick={handleVendorSubmit}
+              >
+                Set Vendor Count
+              </button>
+            </div>
+            <div className="mb-3">
+              <label className="form-label">Customer Count</label>
+              <input
+                type="text"
+                className="form-control"
+                value={customerCount}
+                onChange={handleInputChange(setCustomerCount)}
+              />
+              <button
+                type="button"
+                className="btn btn-secondary mt-2"
+                onClick={handleCustomerSubmit}
+              >
+                Set Customer Count
+              </button>
+            </div>
+          </div>
         </div>
 
-        <div className="col  p-5">
+        <div className="col p-5">
           <LogDisplay />
         </div>
         <div className="col p-5">
--- src/components/ButtonConfig.tsx
+++ src/components/ButtonConfig.tsx
@@ -1,21 +1,12 @@
-import React from 'react'
-import axios from 'axios';
+import React from "react";
+import axios from "axios";
 
 interface Props {
   children: string;
 }
 
-export const ButtonConfig = ({children}:Props) => {
-  const getConfig = () => {
-    try {
-      const response = axios.post("http://localhost:8081/api/ticket-configuration/config")
-    } catch (error) {
-      
-    }
-  };
-  return (
-    <button className="btn btn-primary mt-3" >{children}</button>
-  )
-}
+export const ButtonConfig = ({ children }: Props) => {
+  return <button className="btn btn-primary mt-3">{children}</button>;
+};
 
 export default ButtonConfig;
